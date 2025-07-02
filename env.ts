1f181f30644109780f07590badf7f87fc96b77d9
XandaoPe <73856144+XandaoPe@users.noreply.github.com>
2025-07-02 16:55:33 -0300

    3 commits

--- env.ts
+++
@@ -1,3 +0,0 @@
-export const PORT='3000'
-export const DATABASE_URL='mongodb+srv://alexandredellanno:Xela-2208@cluster0.gaixo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-// export const DATABASE_URL='mongodb+srv://mota:xela2208@motacluster.vdoop.mongodb.net/?retryWrites=true&w=majority&appName=MotaCluster'
